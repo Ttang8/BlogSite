22374263c6e46ab1c6498c1262c1a4947c97407a
Terrence Tang <thtang28@gmail.com>
2018-02-08 11:00:48 -0800

    add fronend react

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <h1>
+      hello this is from app
+    </h1>
+  </div>
+);
+
+export default App;
