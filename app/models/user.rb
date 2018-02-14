92ee3d5dae597bcfabc579b14681d50d5596e1e1
Terrence Tang <thtang28@gmail.com>
2018-02-13 23:49:32 -0800

    add posts to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6, allow_nil: true}
   validates :username, length: {minimum: 4}
 
+  has_many :posts
+
   after_initialize :ensure_session_token
   attr_reader :password
 
